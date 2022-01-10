1ec455e96140f25b16d4a9202382d0bf1357ef25
Jean-Louis Leysens <jloleysens@gmail.com>
2022-01-10 16:29:23 +0200

    clean up unused imports

--- src/components/flex-group.tsx
+++ src/components/flex-group.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import styled from 'styled-components';
 
 export const FlexGroup = styled.div`
   display: flex;
   flex-direction: ${props => props.direction};
+  align-items: ${props => props.alignItems ?? 'flex-start'};
   justify-content: center;
-  align-items: flex-start;
 `;
